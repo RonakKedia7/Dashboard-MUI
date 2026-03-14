61c103c9a6a46bcf897210353ede3d63b264afd0
RonakKedia7 <ronakkedia0@gmail.com>
2026-03-14 14:00:00 +0530

    Added calendar page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Contacts from "./scenes/contacts/Contacts";
 import Form from "./scenes/form/Form";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/Calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -28,11 +29,11 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
